9a96f15fba91e878f8220b9482d5e0948d146608
Loïc Souverain <loic.souverain@gmail.com>
2023-09-27 21:55:13 +0200

    day01

---
+++ day01.py
@@ -0,0 +1,17 @@
+with open("input/day01.txt") as f:
+    masses = [int(l.strip("\n"))//3-2 for l in f.readlines()]
+
+
+print(sum(masses))
+
+masses2 = []
+for mass in masses:
+    t = mass
+    m = mass
+    while m//3 - 2 >= 0:
+        m = m//3 - 2
+        t += m
+    masses2.append(t)
+
+print(sum(masses2))
+    
